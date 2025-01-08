ed1d1e986575e6f156ff5137b0abc6c8dbeb64ed
rayhan889 <raydev1604@gmail.com>
2025-01-09 00:04:41 +0700

    Change web title

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className='py-24'>
       <div className='container'>
-        <h1 className='text-3xl font-bold'>NextTS Starter Project</h1>
+        <h1 className='text-3xl font-bold'>Syntesa Web</h1>
       </div>
     </section>
   )
