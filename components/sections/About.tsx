cb4d2e895faf21e1f31983e6ac88720343c79d9d
rayhan889 <raydev1604@gmail.com>
2025-01-31 17:34:46 +0700

    improve about section

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,12 +1,43 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
 import Image from 'next/image'
 
 const About = () => {
+  const images = [
+    {
+      id: 1,
+      src: 'about_img1.jpg'
+    },
+    {
+      id: 2,
+      src: 'about_img2.jpg'
+    }
+  ]
+
+  const [imageIndex, setImageIndex] = useState<number>(0)
+  const [isFading, setIsFading] = useState<boolean>(false)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsFading(true)
+
+      setTimeout(() => {
+        setImageIndex(prevIndex => (prevIndex + 1) % images.length)
+        setIsFading(false)
+      }, 500)
+    }, 5000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <section className='h-[85%] w-full md:h-1/2' id='about-section'>
       <div className='container mx-auto h-full max-w-7xl py-24'>
         <div className='grid h-full grid-rows-2 md:grid-cols-3'>
           <div className='row-span-2 h-full md:col-span-2'>
-            <div className='flex h-full items-center'>
+            <div className='flex h-full items-center justify-center'>
               <div className='flex flex-col items-start'>
                 <div className='mb-6 space-y-2'>
                   <span className='font-semibold text-gray-300'>ABOUT</span>
@@ -27,16 +58,26 @@ const About = () => {
             </div>
           </div>
           <div className='row-span-2'>
-            <div className='flex h-full items-center'>
+            <div className='flex h-full flex-col items-center justify-center space-y-3'>
               <Image
-                src='/assets/about_img2.jpg'
+                src={`/assets/${images[imageIndex].src}`}
                 alt='About Image'
                 width={400}
                 priority
                 quality={100}
                 height={400}
-                className='h-[85%] w-full rounded-xl object-cover md:h-full lg:h-[80%]'
+                className={`h-[65%] w-full rounded-xl object-cover transition-opacity duration-500 ease-in-out ${
+                  isFading ? 'opacity-0' : 'opacity-100'
+                } md:h-full lg:h-[80%]`}
               />
+              <div className='flex w-full items-center justify-center gap-x-2'>
+                {images.map(img => (
+                  <span
+                    key={img.id}
+                    className={`h-2 w-2 rounded-full ${imageIndex === img.id - 1 ? 'bg-gray-500' : 'bg-gray-300'}`}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
